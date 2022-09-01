1a511e2a6c4506b4c6f5ddd8aa526a0d4cd551cc
karimamanil1 <karimasitimasna@gmail.com>
2022-09-01 17:25:28 +0700

    api usia, status ppns, rev/kepegawaian

--- routes/dashboard/kepegawaian/kepegawaian.js
+++ routes/dashboard/kepegawaian/kepegawaian.js
@@ -240,199 +240,60 @@ module.exports = async function (fastify, opts) {
         }
     );
 
-    // fastify.get(
-    //     "/findone/:id_pegawai", {
-    //         schema: {
-    //             description: "This is an endpoint for fetching a kepegawaian by id",
-    //             tags: ["kepegawaian"],
-    //             params: {
-    //                 description: "Find one kepegawaian by Id",
-    //                 type: "object",
-    //                 properties: {
-    //                     id_pegawai: {
-    //                         type: "string"
-    //                     },
-    //                 },
-    //             },
-    //             response: {
-    //                 200: {
-    //                     description: "Success Response",
-    //                     type: "object",
-    //                     properties: {
-    //                         id: {
-    //                             type: "string"
-    //                         },
-    //                         id_pegawai: {
-    //                             type: "string"
-    //                         },
-    //                         status_kepegawaian: {
-    //                             type: "string"
-    //                         },
-    //                         pendidikan_terakhir: {
-    //                             type: "string"
-    //                         },
-    //                         golongan: {
-    //                             type: "string"
-    //                         },
-    //                         eselon: {
-    //                             type: "string"
-    //                         },
-    //                         jenis_kediklatan: {
-    //                             type: "string"
-    //                         },
-    //                         usia: {
-    //                             type: "number"
-    //                         },
-    //                         usia_pensiun: {
-    //                             type: "number"
-    //                         },
-    //                         status_ppns: {
-    //                             type: "string"
-    //                         }
-    //                     },
-    //                 },
-    //             },
-    //         },
-    //     },
-    //     async (request, reply) => {
-    //         const {
-    //             id_pegawai
-    //         } = request.params;
-    //         const exec = await fastify.kepegawaian.findone(id_pegawai);
-    //         return exec;
-    //     }
-    // );
+    // ^ usia
+    fastify.get(
+        "/sum-usia", {
+            schema: {
+                description: "This is an endpoint for fetching all kepegawaian per usia",
+                tags: ["kepegawaian"],
+                response: {
+                    200: {
+                        description: "Success Response",
+                        type: "array",
+                        properties: {
+                            range_umur: {
+                                type: "string"
+                            },
+                            jumlah: {
+                                type: "number"
+                            }
+                        },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            const exec = await fastify.kepegawaian.get_usia();
+            reply.code(200).send(exec);
+        }
+    );
 
-    // fastify.put(
-    //     "/update/:id_pegawai", {
-    //         schema: {
-    //             description: "This is an endpoint for updating an existing kepegawaian",
-    //             tags: ["kepegawaian"],
-    //             params: {
-    //                 description: "Kepegawaian risiko by Id",
-    //                 type: "object",
-    //                 properties: {
-    //                     id_pegawai: {
-    //                         type: "string"
-    //                     }
-    //                 },
-    //             },
-    //             body: {
-    //                 description: "Payload for updating a kepegawaian",
-    //                 type: "object",
-    //                 properties: {
-    //                     status_kepegawaian: {
-    //                         type: "string"
-    //                     },
-    //                     pendidikan_terakhir: {
-    //                         type: "string"
-    //                     },
-    //                     golongan: {
-    //                         type: "string"
-    //                     },
-    //                     eselon: {
-    //                         type: "string"
-    //                     },
-    //                     jenis_kediklatan: {
-    //                         type: "string"
-    //                     },
-    //                     usia: {
-    //                         type: "number"
-    //                     },
-    //                     usia_pensiun: {
-    //                         type: "number"
-    //                     },
-    //                     status_ppns: {
-    //                         type: "string"
-    //                     }
-    //                 },
-    //             },
-    //             response: {
-    //                 200: {
-    //                     description: "Success Response",
-    //                     type: "object",
-    //                     properties: {
-    //                         status_kepegawaian: {
-    //                             type: "string"
-    //                         },
-    //                         pendidikan_terakhir: {
-    //                             type: "string"
-    //                         },
-    //                         golongan: {
-    //                             type: "string"
-    //                         },
-    //                         eselon: {
-    //                             type: "string"
-    //                         },
-    //                         jenis_kediklatan: {
-    //                             type: "string"
-    //                         },
-    //                         usia: {
-    //                             type: "number"
-    //                         },
-    //                         usia_pensiun: {
-    //                             type: "number"
-    //                         },
-    //                         status_ppns: {
-    //                             type: "string"
-    //                         }
-    //                     },
-    //                 },
-    //             },
-    //         },
-    //     },
-    //     async (request, reply) => {
-    //         const {
-    //             id_pegawai
-    //         } = request.params;
-    //         const {
-    //             status_kepegawaian,
-    //             pendidikan_terakhir,
-    //             golongan,
-    //             eselon,
-    //             jenis_kediklatan,
-    //             usia,
-    //             usia_pensiun,
-    //             status_ppns
-    //         } = request.body;
+    // ^ status ppns
+    fastify.get(
+        "/sum-status-ppns", {
+            schema: {
+                description: "This is an endpoint for fetching all kepegawaian per status ppns",
+                tags: ["kepegawaian"],
+                response: {
+                    200: {
+                        description: "Success Response",
+                        type: "array",
+                        properties: {
+                            status_ppns: {
+                                type: "string"
+                            },
+                            count: {
+                                type: "number"
+                            }
+                        },
+                    },
+                },
+            },
+        },
+        async (request, reply) => {
+            const exec = await fastify.kepegawaian.get_status_ppns();
+            reply.code(200).send(exec);
+        }
+    );
 
-    //         const exec = await fastify.kepegawaian.update(
-    //             status_kepegawaian, pendidikan_terakhir, golongan, eselon, jenis_kediklatan, usia, usia_pensiun, status_ppns
-    //         );
-
-    //         return exec;
-    //     }
-    // );
-
-    // fastify.delete(
-    //     "/delete/:id_pegawai", {
-    //         schema: {
-    //             description: "This is an endpoint for DELETING an existing kepegawaian.",
-    //             tags: ["kepegawaian"],
-    //             params: {
-    //                 description: "delete kepegawaian by id_pegawai",
-    //                 type: "object",
-    //                 properties: {
-    //                     id_pegawai: {
-    //                         type: "string"
-    //                     },
-    //                 },
-    //             },
-    //             response: {
-    //                 204: {
-    //                     type: "string",
-    //                     default: "No Content",
-    //                 },
-    //             },
-    //         },
-    //     },
-    //     async (request, reply) => {
-    //         const {
-    //             id_pegawai
-    //         } = request.params;
-    //         await fastify.kepegawaian.del(id);
-
-    //         reply.status(204);
-    //     }
-    // );
 };
--- services/dashboard/kepegawaian/kepegawaian.js
+++ services/dashboard/kepegawaian/kepegawaian.js
@@ -28,58 +28,38 @@ const kepegawaian = (db) => {
 
     const get_pendidikan_terakhir = () => {
         const query = db.any(
-            "SELECT pendidikan_terakhir, COUNT(pendidikan_terakhir) FROM dashboard_kepegawaian GROUP BY pendidikan_terakhir;"
+            "SELECT pendidikan_terakhir, COUNT(pendidikan_terakhir) FROM dashboard_kepegawaian WHERE NOT status_kepegawaian='PPNS' GROUP BY pendidikan_terakhir;"
         );
         return query;
     };
 
     const get_golongan = () => {
         const query = db.any(
-            "SELECT golongan, COUNT( golongan) FROM dashboard_kepegawaian GROUP BY golongan;"
+            "SELECT golongan, COUNT( golongan) FROM dashboard_kepegawaian WHERE NOT status_kepegawaian='PPNS' GROUP BY golongan;"
         );
         return query;
     };
 
     const get_eselon = () => {
         const query = db.any(
-            "SELECT eselon, COUNT( eselon) FROM dashboard_kepegawaian GROUP BY eselon;"
+            "SELECT eselon, COUNT( eselon) FROM dashboard_kepegawaian WHERE NOT status_kepegawaian='PPNS' GROUP BY eselon;"
         );
         return query;
     };
 
-    // const get_total_pegawai_kecamatan = () => {
-    //     const query = db.one(
-    //         "SELECT COUNT(id_pegawai) as jumlah FROM kepegawaian WHERE is_deleted = 0"
-    //     );
-    //     return query;
-    // };
+    const get_usia = () => {
+        const query = db.any(
+            "SELECT CASE WHEN usia BETWEEN 0 and 20 THEN '0 - 20'  WHEN usia BETWEEN 21 and 25 THEN '21 - 25'  WHEN usia BETWEEN 26 and 30 THEN '26 - 30' WHEN usia BETWEEN 31 and 35 THEN '31 - 35' WHEN usia BETWEEN 36 and 40 THEN '36 - 40' WHEN usia BETWEEN 41 and 45 THEN '41 - 45' WHEN usia BETWEEN 46 and 50 THEN '46 - 50' WHEN usia BETWEEN 51 and 55 THEN '51 - 55' WHEN usia BETWEEN 56 and 60 THEN '56 - 60' END as range_umur,  COUNT(*) AS jumlah FROM dashboard_kepegawaian WHERE NOT status_kepegawaian='PPNS' GROUP BY range_umur"
+        );
+        return query;
+    };
 
-    // const findone = (id_pegawai) => {
-    //     const query = db.one(
-    //         "SELECT id_pegawai, status_kepegawaian, pendidikan_terakhir, golongan, eselon, jenis_kediklatan, usia, usia_pensiun, status_ppns FROM kepegawaian WHERE id_pegawai = $1",
-    //         [id_pegawai]
-    //     );
-    //     return query;
-    // };
-
-    // const update = (id_pegawai) => {
-    //     db.one(
-    //         "UPDATE kepegawaian SET status_kepegawaian, pendidikan_terakhir, golongan, eselon, jenis_kediklatan, usia, usia_pensiun, status_ppns = $1, $2, $3, $4, $5, $6, $7, $8, updated_at = CURRENT_TIMESTAMP WHERE id_pegawai= $9 RETURNING id_pegawai",
-    //         [status_kepegawaian, pendidikan_terakhir, golongan, eselon, jenis_kediklatan, usia, usia_pensiun, status_ppns]
-    //     );
-    // };
-
-    // const del = async (id_pegawai) => {
-    //     await db.one(
-    //         "UPDATE kepegawaian SET is_deleted = 1, deleted_at = CURRENT_TIMESTAMP WHERE id_pegawai = $1 RETURNING id_pegawai",
-    //         [id_pegawai]
-    //     );
-
-    //     return {
-    //         id_pegawai
-    //     };
-    // };
-
+    const get_status_ppns = () => {
+        const query = db.any(
+            "SELECT status_ppns, COUNT( status_ppns) FROM dashboard_kepegawaian WHERE status_kepegawaian='PPNS'  GROUP BY status_ppns"
+        );
+        return query;
+    };
 
     return {
         create,
@@ -87,11 +67,9 @@ const kepegawaian = (db) => {
         get_status_kepegawaian,
         get_pendidikan_terakhir,
         get_golongan,
-        get_eselon
-        // findone,
-        // update,
-        // del,
-
+        get_eselon,
+        get_usia,
+        get_status_ppns
     };
 };
 
